e1bd3723b8827dc04bd0c43ddbe30d090c0c466e
gleb87 <gleb87@3g.ua>
2015-03-05 13:23:41 +0200

    start

---
+++ modalWindow.js
@@ -0,0 +1,16 @@
+function ModalWindow (options) {
+	var self = this;
+
+	var template = _.template(options.template);
+	var elem;
+
+	self.getElement = function() {
+		if (!elem) render();
+
+		return elem;
+	}
+
+	function render() {
+		elem = $(template());
+	}
+}
